0887f35836032f2866a404de4d1df824220a5566
Caio Almeida Santos <aceutoxzc@gmail.com>
2023-08-20 12:33:40 -0300

    trocando de ide

--- gerenciamento/src/main/java/br/com/gerenciamento/controllers/MateriaController.java
+++ gerenciamento/src/main/java/br/com/gerenciamento/controllers/MateriaController.java
@@ -1,9 +1,11 @@
 package br.com.gerenciamento.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gerenciamento.dao.MateriaDao;
 
+@RestController
 public class MateriaController {
     
     @Autowired
@@ -11,3 +13,4 @@ public class MateriaController {
 
     
 }
+ 
